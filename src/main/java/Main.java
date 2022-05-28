2467a0ff82608f74f9526a7e58e69ca05cb14ce6
Anton Illarionov <anton.illarionov1991@gmail.com>
2022-05-28 17:17:34 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.pasport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.number = "+7 (999)-999-99-99";
+        post.surmane = "Иванов";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String pasport;
+    public String patronymic;
+    public String number;
+    public String surmane;
+    public boolean subscription;
+    public FormDate  birthday;
+}
